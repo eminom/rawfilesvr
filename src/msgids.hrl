cc6efd9154ba06932041c114887ded170b7adf16
eminom <eminem7409@163.com>
2015-06-30 14:42:54 +0800

    add missing files

---
+++ src/msgids.hrl
@@ -0,0 +1,25 @@
+
+%% This file shall be generated automatically.
+
+-ifndef(MSGID_Directory).
+-define(MSGID_Directory, true).
+
+-define(MsgID_RequestWorldList,    1001).
+-define(MsgID_RequestUserRegister, 1002).
+
+-define(MsgID_ResponseWorldList,    101001).
+-define(MsgID_ResponseUserRegister, 101002).
+
+-endif.
+
+
+-ifndef(MSGID_World).
+-define(MSGID_World, true).
+
+-define(MsgID_RequestLogin,        2001).
+-define(MsgID_RequestCreatePlayer, 2002).
+
+-define(MsgID_ResponseLogin,        102001).
+-define(MsgID_ResponseCreatePlayer, 102002).
+
+-endif.
